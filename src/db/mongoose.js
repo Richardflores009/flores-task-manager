909680ac6cd324b089ce7faeb488d839726284ce
Dick Flowers <dickflowers@DickMiniPro.local>
2021-02-20 17:11:40 -0700

    added async functions

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -4,7 +4,8 @@ const mongoose = require('mongoose')
 mongoose.connect('mongodb://127.0.0.1:27017/myproject-api', {
     useNewUrlParser: true,
     useCreateIndex: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify: false
 })
 
 
